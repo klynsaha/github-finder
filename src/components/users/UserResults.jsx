84ed317d306cec5cde16d8cd61b4268e7d4fd5a2
Kalyan Saha <klynsaha@gmail.com>
2022-01-01 05:27:21 +0530

    Initial Commit

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useContext } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
+
+function UserResults() {
+    const { isLoading, users } = useContext(GithubContext);
+
+    return isLoading ? (
+        <Spinner />
+    ) : (
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+            {users.map((user) => (
+                <UserItem key={user.id} user={user} />
+            ))}
+        </div>
+    );
+}
+
+export default UserResults;
